4bcfbc207a50ef0b5ddbbb8dcbf021960a3eddaf
Dasara Thanmai sri sathvika <sathvikadasara06@gmail.com>
2025-05-25 20:05:38 +0530

    Add files via upload

---
+++ list_methods.py
@@ -0,0 +1,13 @@
+myListOfNumbers = [1,8,7,2,21,15]
+
+print(myListOfNumbers)
+
+#myListOfNumbers.sort() # Sorts the original list 3
+#myListOfNumbers.reverse() # Reverses the original list 4
+#myListOfNumbers.append(9) # Add 9 at the end of the original list
+#myListOfNumbers.insert(2, 9) # Inserts 9 at index 2
+#myListOfNumbers.pop() # Removes an item from the end of the list
+#myListOfNumbers.pop(2) # Removes an item from the given index from the list
+
+myListOfNumbers.remove(21) # Removes a given item from the list
+print(myListOfNumbers)
