76eb944a15f9f749659f8cc8942adb16685d3368
Dasara Thanmai sri sathvika <sathvikadasara06@gmail.com>
2025-05-25 20:05:04 +0530

    Add files via upload

---
+++ str_func.py
@@ -0,0 +1,11 @@
+mystr="My name is harry"
+
+print(len(mystr))
+print(mystr.endswith("xyz"))
+print(mystr.startswith("My"))
+print(mystr.count('ab'))
+print(mystr.capitalize())
+
+myStr="my name is harry name"
+print(myStr.find("name"))
+print(myStr.replace("name","date"))
