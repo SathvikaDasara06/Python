e3914c9f9ddd9f4560726f175bced241774c4b6f
Dasara Thanmai sri sathvika <sathvikadasara06@gmail.com>
2025-06-11 15:10:59 +0530

    Add files via upload

---
+++ formatstr.py
@@ -0,0 +1,13 @@
+person = 'Jeff'
+
+pet_name = 'Rex'
+
+pet_type = 'dog'
+
+print (person, 'has a', pet_type, 'and his name is', pet_name, '.')
+
+print (f'{person} has a {pet_type} and his name is {pet_name}.')
+
+print('{} has a {} and his name is {}'.format(person, pet_type, pet_name))
+
+print('%s has a %s and his name is %s.' % (person, pet_type, pet_name))
